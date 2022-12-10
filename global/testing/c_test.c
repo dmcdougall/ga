706259d39ca38039e37bec321981d7e7d1ee4878
Damon McDougall <damon.mcdougall@amd.com>
2022-12-09 23:44:41 -0500

    Remove useless code

--- global/testing/c_test.c
+++ global/testing/c_test.c
@@ -68,67 +68,6 @@ void device_free(void *buf) {
 }
 
 
-void factor(int p, int *idx, int *idy) {
-  int i, j;                              
-  int ip, ifac, pmax;                    
-  int prime[MAX_FACTOR];                 
-  int fac[MAX_FACTOR];                   
-  int ix, iy;                            
-  int ichk;                              
-
-  i = 1;
-
- //find all prime numbers, besides 1, less than or equal to the square root of p
-  ip = (int)(sqrt((double)p))+1;
-
-  pmax = 0;
-  for (i=2; i<=ip; i++) {
-    ichk = 1;
-    for (j=0; j<pmax; j++) {
-      if (i%prime[j] == 0) {
-        ichk = 0;
-        break;
-      }
-    }
-    if (ichk) {
-      pmax = pmax + 1;
-      if (pmax > MAX_FACTOR) printf("Overflow in grid_factor\n");
-      prime[pmax-1] = i;
-    }
-  }
-
- //find all prime factors of p
-  ip = p;
-  ifac = 0;
-  for (i=0; i<pmax; i++) {
-    while(ip%prime[i] == 0) {
-      ifac = ifac + 1;
-      fac[ifac-1] = prime[i];
-      ip = ip/prime[i];
-    }
-  }
-
- //when p itself is prime
-  if (ifac==0) {
-    ifac++;
-    fac[0] = p;
-  }
-
-
- //find two factors of p of approximately the same size
-  *idx = 1;
-  *idy = 1;
-  for (i = ifac-1; i >= 0; i--) {
-    ix = *idx;
-    iy = *idy;
-    if (ix <= iy) {
-      *idx = fac[i]*(*idx);
-    } else {
-      *idy = fac[i]*(*idy);
-    }
-  }
-}
-
 //int nprocs, rank, wrank;
 int nprocs, rank;
 int pdx, pdy;
@@ -160,6 +99,9 @@ void test_int_array(int on_device, int local_buf_on_device)
   int *ptr;
   int one;
   double zero = 0.0;
+
+  pdx = 2;
+  pdy = 2;
 
   ndim = 2;
   dims[0] = DIMSIZE;
@@ -196,49 +138,33 @@ void test_int_array(int on_device, int local_buf_on_device)
 
   /* allocate a local buffer and initialize it with values*/
   nsize = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-  if (local_buf_on_device) {
-    void *tbuf;
-    set_device(&my_dev);
-    device_malloc(&tbuf,(int)(nsize*sizeof(int)));
-    buf = (int*)tbuf;
-  } else {
-    buf = (int*)malloc(nsize*sizeof(int));
-  }
+  set_device(&my_dev);
+  device_malloc(&tbuf,(int)(nsize*sizeof(int)));
+  buf = (int*)tbuf;
 
   for (n=0; n<NLOOP; n++) {
     GA_Zero(g_a);
     GA_Fill(g_a,&zero);
     ld = (hi[1]-lo[1]+1);
-    if (local_buf_on_device) {
-      if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-        int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-        tbuf = (int*)malloc(tnelem*sizeof(int));
-        for (ii = lo[0]; ii<=hi[0]; ii++) {
-          i = ii-lo[0];
-          for (jj = lo[1]; jj<=hi[1]; jj++) {
-            j = jj-lo[1];
-            idx = i*ld+j;
-            tbuf[idx] = ii*DIMSIZE+jj;
-          }
-        }
-        memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-        free(tbuf);
-      }
-    } else {
+    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+      tbuf = (int*)malloc(tnelem*sizeof(int));
       for (ii = lo[0]; ii<=hi[0]; ii++) {
         i = ii-lo[0];
         for (jj = lo[1]; jj<=hi[1]; jj++) {
           j = jj-lo[1];
           idx = i*ld+j;
-          buf[idx] = ii*DIMSIZE+jj;
+          tbuf[idx] = ii*DIMSIZE+jj;
         }
       }
+      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
+      free(tbuf);
     }
     /* copy data to global array */
     NGA_Put(g_a, lo, hi, buf, &ld);
     GA_Sync();
     NGA_Distribution(g_a,rank,tlo,thi);
-#if 1
+
     if (rank == 0 && n == 0) printf("Completed NGA_Distribution\n");
     if (tlo[0]<=thi[0] && tlo[1]<=thi[1]) {
       int tnelem = (thi[0]-tlo[0]+1)*(thi[1]-tlo[1]+1);
@@ -263,72 +189,46 @@ void test_int_array(int on_device, int local_buf_on_device)
       }
     }
     GA_Sync();
-#endif
 
     /* zero out local buffer */
-    if (local_buf_on_device) {
-      int *tbuf = (int*)malloc(nsize*sizeof(int));
-      for (i=0; i<nsize; i++) tbuf[i] = 0;
-      memcpyH2D(buf, tbuf, nsize*sizeof(int));
-      free(tbuf);
-    } else {
-      for (i=0; i<nsize; i++) buf[i] = 0;
-    }
+    int *tbuf = (int*)malloc(nsize*sizeof(int));
+    for (i=0; i<nsize; i++) tbuf[i] = 0;
+    memcpyH2D(buf, tbuf, nsize*sizeof(int));
+    free(tbuf);
 
     /* copy data from global array to local buffer */
     NGA_Get(g_a, lo, hi, buf, &ld);
     GA_Sync();
 
-    if (local_buf_on_device) {
-      if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-        int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-        tbuf = (int*)malloc(tnelem*sizeof(int));
-        for (i=0; i<tnelem; i++) tbuf[i] = 0;
-        memcpyD2H(tbuf, buf, tnelem*sizeof(int));
-        for (ii = lo[0]; ii<=hi[0]; ii++) {
-          i = ii-lo[0];
-          for (jj=lo[1]; jj<=hi[1]; jj++) {
-            j = jj-lo[1];
-            idx = i*ld+j;
-          }
-        }
-        free(tbuf);
-      }
-    } else {
+    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+      tbuf = (int*)malloc(tnelem*sizeof(int));
+      for (i=0; i<tnelem; i++) tbuf[i] = 0;
+      memcpyD2H(tbuf, buf, tnelem*sizeof(int));
       for (ii = lo[0]; ii<=hi[0]; ii++) {
         i = ii-lo[0];
-        for (jj = lo[1]; jj<=hi[1]; jj++) {
+        for (jj=lo[1]; jj<=hi[1]; jj++) {
           j = jj-lo[1];
           idx = i*ld+j;
         }
       }
+      free(tbuf);
     }
 
     /* reset values in buf */
-    if (local_buf_on_device) {
-      if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-        int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-        tbuf = (int*)malloc(tnelem*sizeof(int));
-        for (ii = lo[0]; ii<=hi[0]; ii++) {
-          i = ii-lo[0];
-          for (jj = lo[1]; jj<=hi[1]; jj++) {
-            j = jj-lo[1];
-            idx = i*ld+j;
-            tbuf[idx] = ii*DIMSIZE+jj;
-          }
-        }
-        memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-        free(tbuf);
-      }
-    } else {
+    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+      tbuf = (int*)malloc(tnelem*sizeof(int));
       for (ii = lo[0]; ii<=hi[0]; ii++) {
         i = ii-lo[0];
         for (jj = lo[1]; jj<=hi[1]; jj++) {
           j = jj-lo[1];
           idx = i*ld+j;
-          buf[idx] = ii*DIMSIZE+jj;
+          tbuf[idx] = ii*DIMSIZE+jj;
         }
       }
+      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
+      free(tbuf);
     }
 
     /* accumulate data to global array */
@@ -336,58 +236,33 @@ void test_int_array(int on_device, int local_buf_on_device)
     NGA_Acc(g_a, lo, hi, buf, &ld, &one);
     GA_Sync();
     /* reset values in buf */
-    if (local_buf_on_device) {
-      if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-        int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-        tbuf = (int*)malloc(tnelem*sizeof(int));
-        for (i=0; i<nelem; i++) tbuf[i] = 0;
-        memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-        free(tbuf);
-      }
-    } else {
-      for (ii = lo[0]; ii<=hi[0]; ii++) {
-        i = ii-lo[0];
-        for (jj = lo[1]; jj<=hi[1]; jj++) {
-          j = jj-lo[1];
-          idx = i*ld+j;
-          buf[idx] = 0;
-        }
-      }
+    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+      tbuf = (int*)malloc(tnelem*sizeof(int));
+      for (i=0; i<nelem; i++) tbuf[i] = 0;
+      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
+      free(tbuf);
     }
 
     NGA_Get(g_a, lo, hi, buf, &ld);
     GA_Sync();
-    if (local_buf_on_device) {
-      if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-        int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-        tbuf = (int*)malloc(tnelem*sizeof(int));
-        for (i=0; i<tnelem; i++) tbuf[i] = 0;
-        memcpyD2H(tbuf, buf, tnelem*sizeof(int));
-        for (ii = lo[0]; ii<=hi[0]; ii++) {
-          i = ii-lo[0];
-          for (jj=lo[1]; jj<=hi[1]; jj++) {
-            j = jj-lo[1];
-            idx = i*ld+j;
-          }
-        }
-        free(tbuf);
-      }
-    } else {
+    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+      tbuf = (int*)malloc(tnelem*sizeof(int));
+      for (i=0; i<tnelem; i++) tbuf[i] = 0;
+      memcpyD2H(tbuf, buf, tnelem*sizeof(int));
       for (ii = lo[0]; ii<=hi[0]; ii++) {
         i = ii-lo[0];
-        for (jj = lo[1]; jj<=hi[1]; jj++) {
+        for (jj=lo[1]; jj<=hi[1]; jj++) {
           j = jj-lo[1];
           idx = i*ld+j;
         }
       }
+      free(tbuf);
     }
   }
 
-  if (local_buf_on_device) {
-    device_free(buf);
-  } else {
-    free(buf);
-  }
+  device_free(buf);
   GA_Destroy(g_a);
 }
 
@@ -403,11 +278,6 @@ int main(int argc, char **argv) {
   int i;
   
   MPI_Init(&argc, &argv);
-
-  int myrank, mysize;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-  MPI_Comm_size(MPI_COMM_WORLD, &mysize);
-
   MA_init(C_DBL, 2000000, 2000000);
   GA_Initialize();
 
@@ -422,19 +292,14 @@ int main(int argc, char **argv) {
   devIDs = (int*)malloc(nprocs*sizeof(int));
   NGA_Device_host_list(list, devIDs, &ndev, NGA_Pgroup_get_default());
 
-  local_buf_on_device = 0;
   for (i=0; i<ndev; i++) {
      if (rank == list[i]) {
-       local_buf_on_device = 1;
        my_dev = devIDs[i];
        break;
      }
   }
 
-  /* Divide matrix up into pieces that are owned by each processor */
-  factor(nprocs, &pdx, &pdy);
-  if (rank == 0) printf("  Testing integer array on device, local buffer on device\n");
-  test_int_array(1,local_buf_on_device);
+  test_int_array(1,1);
 
   free(list);
   free(devIDs);
