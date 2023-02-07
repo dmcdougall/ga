690ea42b08c79e17a34897481d149c7c712931f1
Damon McDougall <damon.mcdougall@amd.com>
2023-02-06 19:50:59 -0500

    Remove a lot more unneeded code

--- global/testing/c_test.c
+++ global/testing/c_test.c
@@ -17,30 +17,7 @@
 #define SMLDIM 256
 #define MAXCOUNT 10000
 #define MAX_FACTOR 256
-#define NLOOP 10
-
-void set_device(int* devid) {
-  hipSetDevice(*devid);
-}
-
-void device_malloc(void **buf, size_t bsize){
-  hipMalloc(buf, bsize);
-}
-
-void memcpyH2D(void* dbuf, void* sbuf, size_t bsize){
-  hipMemcpy(dbuf, sbuf, bsize, hipMemcpyHostToDevice);
-  hipDeviceSynchronize();
-}
-
-void memcpyD2H(void* dbuf, void* sbuf, size_t bsize){
-  hipMemcpy(dbuf, sbuf, bsize, hipMemcpyDeviceToHost);
-  hipDeviceSynchronize();
-}
-
-void device_free(void *buf) {
-  hipFree(buf);
-}
-
+#define NLOOP 1
 
 int rank;
 int pdx, pdy;
@@ -107,125 +84,38 @@ void test_int_array()
 
   /* allocate a local buffer and initialize it with values*/
   nsize = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-  set_device(&my_dev);
-  device_malloc((void**)&tbuf,(int)(nsize*sizeof(int)));
+  hipSetDevice(my_dev);
+
+  hipMalloc((void**)&tbuf,(int)(nsize*sizeof(int)));
   buf = (int*)tbuf;
 
-  for (n=0; n<NLOOP; n++) {
-    GA_Zero(g_a);
-    GA_Fill(g_a,&zero);
-    ld = (hi[1]-lo[1]+1);
-    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      for (ii = lo[0]; ii<=hi[0]; ii++) {
-        i = ii-lo[0];
-        for (jj = lo[1]; jj<=hi[1]; jj++) {
-          j = jj-lo[1];
-          idx = i*ld+j;
-          tbuf[idx] = ii*DIMSIZE+jj;
-        }
+  GA_Zero(g_a);
+  GA_Fill(g_a,&zero);
+  ld = (hi[1]-lo[1]+1);
+  if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
+    int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+    tbuf = (int*)malloc(tnelem*sizeof(int));
+    for (ii = lo[0]; ii<=hi[0]; ii++) {
+      i = ii-lo[0];
+      for (jj = lo[1]; jj<=hi[1]; jj++) {
+        j = jj-lo[1];
+        idx = i*ld+j;
+        tbuf[idx] = ii*DIMSIZE+jj;
       }
-      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-      free(tbuf);
     }
-    /* copy data to global array */
-    NGA_Put(g_a, lo, hi, buf, &ld);
-    GA_Sync();
-    NGA_Distribution(g_a,rank,tlo,thi);
+    hipMemcpy(buf, tbuf, tnelem*sizeof(int), hipMemcpyHostToDevice);
+    hipDeviceSynchronize();
 
-    if (rank == 0 && n == 0) printf("Completed NGA_Distribution\n");
-    if (tlo[0]<=thi[0] && tlo[1]<=thi[1]) {
-      int tnelem = (thi[0]-tlo[0]+1)*(thi[1]-tlo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      NGA_Access(g_a,tlo,thi,&ptr,&tld);
-      for (i=0; i<tnelem; i++) tbuf[i] = 0;
-      memcpyD2H(tbuf, ptr, tnelem*sizeof(int));
-      for (ii = tlo[0]; ii<=thi[0]; ii++) {
-        i = ii-tlo[0];
-        for (jj=tlo[1]; jj<=thi[1]; jj++) {
-          j = jj-tlo[1];
-          idx = i*tld+j;
-        }
-      }
-      NGA_Release(g_a,tlo,thi);
-      free(tbuf);
-    }
-    GA_Sync();
-
-    /* zero out local buffer */
-    int *tbuf = (int*)malloc(nsize*sizeof(int));
-    for (i=0; i<nsize; i++) tbuf[i] = 0;
-    memcpyH2D(buf, tbuf, nsize*sizeof(int));
     free(tbuf);
-
-    /* copy data from global array to local buffer */
-    NGA_Get(g_a, lo, hi, buf, &ld);
-    GA_Sync();
-
-    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      for (i=0; i<tnelem; i++) tbuf[i] = 0;
-      memcpyD2H(tbuf, buf, tnelem*sizeof(int));
-      for (ii = lo[0]; ii<=hi[0]; ii++) {
-        i = ii-lo[0];
-        for (jj=lo[1]; jj<=hi[1]; jj++) {
-          j = jj-lo[1];
-          idx = i*ld+j;
-        }
-      }
-      free(tbuf);
-    }
-
-    /* reset values in buf */
-    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      for (ii = lo[0]; ii<=hi[0]; ii++) {
-        i = ii-lo[0];
-        for (jj = lo[1]; jj<=hi[1]; jj++) {
-          j = jj-lo[1];
-          idx = i*ld+j;
-          tbuf[idx] = ii*DIMSIZE+jj;
-        }
-      }
-      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-      free(tbuf);
-    }
-
-    /* accumulate data to global array */
-    one = 1;
-    NGA_Acc(g_a, lo, hi, buf, &ld, &one);
-    GA_Sync();
-    /* reset values in buf */
-    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      for (i=0; i<nelem; i++) tbuf[i] = 0;
-      memcpyH2D(buf, tbuf, tnelem*sizeof(int));
-      free(tbuf);
-    }
-
-    NGA_Get(g_a, lo, hi, buf, &ld);
-    GA_Sync();
-    if (lo[0]<=hi[0] && lo[1]<=hi[1]) {
-      int tnelem = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
-      tbuf = (int*)malloc(tnelem*sizeof(int));
-      for (i=0; i<tnelem; i++) tbuf[i] = 0;
-      memcpyD2H(tbuf, buf, tnelem*sizeof(int));
-      for (ii = lo[0]; ii<=hi[0]; ii++) {
-        i = ii-lo[0];
-        for (jj=lo[1]; jj<=hi[1]; jj++) {
-          j = jj-lo[1];
-          idx = i*ld+j;
-        }
-      }
-      free(tbuf);
-    }
   }
+  /* copy data to global array */
+  NGA_Put(g_a, lo, hi, buf, &ld);
+  GA_Sync();
+  NGA_Distribution(g_a,rank,tlo,thi);
 
-  device_free(buf);
+  if (rank == 0 && n == 0) printf("Completed NGA_Distribution\n");
+
+  hipFree(buf);
   GA_Destroy(g_a);
 }
 
